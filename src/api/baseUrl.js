c1533608e4ef602fa0bd5d063a0e299c72969680
DIxit LUkhi <lukhidixit149@gmail.com>
2024-07-12 16:01:45 +0530

    url change

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
-// export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://applog-backend.onrender.com";
+export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "https://applog-backend.onrender.com";
 // export const s3Url = process.env.REACT_APP_S3_API;
 export const driveUrl = "https://drive.google.com/uc?id=";  
 // export const driveUrl = "https://drive.usercontent.google.com/download?id=";  
